60ff3e9c24b6b312bfed3f03d7f972975d0dd208
tilman <moeller.tilman@gmail.com>
2019-01-18 14:02:00 +0100

    add cors

--- floatbackendapp/src/main/scala/ScalatraBootstrap.scala
+++ floatbackendapp/src/main/scala/ScalatraBootstrap.scala
@@ -4,6 +4,10 @@ import javax.servlet.ServletContext
 
 class ScalatraBootstrap extends LifeCycle {
   override def init(context: ServletContext) {
+    // Optional because * is the default
+    context.initParameters("org.scalatra.cors.allowedOrigins") = "*"
+    // Disables cookies, but required because browsers will not allow passing credentials to wildcard domains
+    context.initParameters("org.scalatra.cors.allowCredentials") = false
     context.mount(new MyScalatraServlet, "/*")
   }
 }
