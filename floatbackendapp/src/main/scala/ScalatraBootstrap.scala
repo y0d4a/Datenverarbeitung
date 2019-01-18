bcb1015432d244db601e2eeeeab2e6f5385aea13
tilman <moeller.tilman@gmail.com>
2019-01-18 14:36:28 +0100

    add header allow orgigin

--- floatbackendapp/src/main/scala/ScalatraBootstrap.scala
+++ floatbackendapp/src/main/scala/ScalatraBootstrap.scala
@@ -6,8 +6,11 @@ class ScalatraBootstrap extends LifeCycle {
   override def init(context: ServletContext) {
     // Optional because * is the default
     context.initParameters("org.scalatra.cors.allowedOrigins") = "*"
+    context.initParameters("org.scalatra.cors.allowedMethods") = "GET"
     // Disables cookies, but required because browsers will not allow passing credentials to wildcard domains
     context.initParameters("org.scalatra.cors.allowCredentials") = "false"
+    // If CorsSupport needs to be disabled, set to false. Default: CorsSupport is enabled.
+    context.initParameters("org.scalatra.cors.enable") = "false"
     context.mount(new MyScalatraServlet, "/*")
   }
 }
