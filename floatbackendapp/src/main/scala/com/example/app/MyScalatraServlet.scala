da3d04f8cd54b072167cdf085865b3752d8f198c
Martin Gladigau <martin.gladigau@computacenter.com>
2019-09-24 13:18:14 +0200

    added 404s

--- floatbackendapp/src/main/scala/com/example/app/MyScalatraServlet.scala
+++ floatbackendapp/src/main/scala/com/example/app/MyScalatraServlet.scala
@@ -1,6 +1,7 @@
 package com.example.app
 
 import com.example.app.storage.BuoyProcessor
+import org.apache.commons.lang.exception.ExceptionUtils.getStackTrace
 import scala.io.Source
 import org.scalatra._
 
@@ -35,21 +36,41 @@ class MyScalatraServlet extends ScalatraServlet with JacksonJsonSupport {
   get("/") {
     contentType = formats("html")
     response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"))
-    Source.fromFile("src/main/resources/html/index.html").getLines.mkString
+    val source = Source.fromFile("src/main/resources/html/index.html")
+    val output = source.getLines().mkString;
+    source.close();
+    output;
   }
 
   get("/last_coordinates") {
     response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"))
-    bprocessor.retrieveCoordinatesAndIDs
+    try {
+      Ok(bprocessor.retrieveCoordinatesAndIDs)
+    }
+    catch {
+      case _: Throwable => InternalServerError()
+    }
   }
 
   get("/measurements/:float_id") {
     response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"))
-    bprocessor.retrievePathAndLastMeasurements(params("float_id"))
+    try {
+      Ok(bprocessor.retrievePathAndLastMeasurements(params("float_id")))
+    }
+    catch {
+      case _: NoSuchElementException => NotFound("This float_id does not exist in our database.")
+      case _: Throwable => InternalServerError()
+    }
   }
 
   get("/measurements/:float_id/:cycle_num") {
     response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"))
-    bprocessor.retrieveMeasurements(params("float_id"), params("cycle_num"))
+    try {
+      Ok(bprocessor.retrieveMeasurements(params("float_id"), params("cycle_num")))
+    }
+    catch {
+      case _: NoSuchElementException => NotFound("This combination of float_id and cycle_num does not exist in our database.")
+      case _: Throwable => InternalServerError()
+    }
   }
 }
